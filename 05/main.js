6dc60a57ff6546baa70581d755e967b9e7fabf0d
Shemet <vitaliy.shemet@it-devgroup.com>
2018-08-11 10:51:21 +0300

    homework 05

---
+++ 05/main.js
@@ -0,0 +1,41 @@
+// 1. Система уравнений
+
+// let a = parseFloat(prompt('Enter a', ''));
+// let b = parseFloat(prompt('Enter b', ''));
+// let x;
+// if (a > b) {
+//   x = a + b / 2 * 4;
+// } else if (a = b) {
+//   x = 400;
+// } else if (a < b) {
+//   x = a - b + 2 / b * 4;
+// }
+// console.log(x);
+
+
+// 3. Дни недели
+let day = parseFloat(prompt('Enter number ', ''));
+ switch (day) {
+   case 1:
+     console.log('понедельник');
+     break;
+   case 2:
+     console.log('вторник');
+     break;
+   case 3:
+     console.log('среда');
+     break;
+   case 4:
+     console.log('четверг');
+     break;
+   case 5:
+     console.log('пятница');
+     break;
+   case 6:
+     console.log('суббота');
+     break;
+   case 7:
+     console.log('воскресенье');
+     break;
+ }
+
