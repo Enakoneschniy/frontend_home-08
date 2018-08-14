e0094570e557342ab9f82a88b479f07c1c73ce4c
fon_harry <fon.harry@gmail.com>
2018-08-14 20:49:37 +0300

    homework 06

---
+++ 06/js/app.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// 1.
+for (let index1 = 4; index1 <= 400; index1++) {
+    console.log(index1);
+}
+
+// 2.
+for (let index2 = 4; index2 <= 13; index2 += 3) {
+    console.log(index2);
+}
+
+// 3.
+for (let index3 = 654; index3 >= 0; index3--) {
+    console.log(index3);
+}
+
+// 4.
+for (let index4 = 1983; index4 <= 2017; index4++) {
+    console.log(index4);
+}
+
+// 5.
+for (let index5 = -4; index5 <= 100; index5 += 2) {
+    console.log(index5);
+}
+
+// 6.
+for (let index6 = 1; index6 <= 9; index6++) {
+    console.log(`${index6} * 7 = ${index6 * 7}`);
+}
+
+// 7.
+for (let index7 = 1000; index7 <= 2000; index7++) {
+    document.write(`&#${index7}<br>`);
+    console.log(`&#${index7}`);
+}
+
+// 8.
+let result8 = 0;
+for (let index8 = 1; index8 <= 100; index8++) {
+    result8 += index8;
+}
+
+console.log(result8);
+
+// 9.
+let result9 = 0;
+for (let index9 = 1; index9 <= 50; index9++) {
+    result9 += index9;
+}
+
+console.log(result9);
