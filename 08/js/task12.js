c2a8f6ba731ae192a967bc0348a23cb20b7b6322
Hannasezen <41483582+Hannasezen@users.noreply.github.com>
2018-08-30 12:42:34 +0300

    task12

--- 08/js/task12.js
+++ 08/js/task12.js
@@ -1,4 +1,14 @@
-( function () {
-    'use strict';
-
-} )();
+(() => {
+'use strict';
+var bigMac = { price: 17, kkal: 250 };
+var smallMac = { price: 25, kkal: 340 };
+var standart = {
+	cheese: { price: 4, kkal: 25},
+	salad: { price: 5, kkal: 5 },
+	ham: { price: 10, kkal: 50 },
+};
+var extra = {
+	sauce: { price: 5, kkal: 0 },
+	mayonnaise: { price: 4, kkal: 10 },
+}
+})();
