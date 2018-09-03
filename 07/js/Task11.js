07a00b90e8e1930679975b11d3ac4f655588fb86
Sezen Anna <41483582+Hannasezen@users.noreply.github.com>
2018-09-03 14:54:31 +0300

    Update Task11.js

--- 07/js/Task11.js
+++ 07/js/Task11.js
@@ -4,7 +4,7 @@ var arr = [15, 12, 1, 259, 65, 48, 778, 13, -88, 6, 14, 77, 103];
 var arrNew = [];
 
 function isEven ( arr ) {
-	for ( i = 0; i < arr.length; i++ ) {
+	for ( var i = 0; i < arr.length; i++ ) {
 		if ( arr[i] % 2 === 0 ) {
 			arrNew.push( arr[i] );
 		}
