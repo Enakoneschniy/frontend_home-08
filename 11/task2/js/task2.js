1a5600dfbfefcdd0268e4f4645f41546c0463427
ivanov0ivanov <ivanov.ivanov8820@gmail.com>
2018-09-04 17:27:02 +0300

    HW

--- 11/task2/js/task2.js
+++ 11/task2/js/task2.js
@@ -1,6 +1,6 @@
 ;(() => {
     'use strict';
     let str = 'var_text_hello';
-    let res =  str.split('_').reduce((acc,item) => acc + (acc === "" ? item : item.slice(0, 1).toUpperCase() + item.slice(1)),"");
+    let res = str.split('_').reduce((acc, item) => acc + (acc === "" ? item : item.slice(0, 1).toUpperCase() + item.slice(1)), "");
     console.log(res);
 })();
---
+++ 11/task3/js/task3.js
@@ -0,0 +1,7 @@
+;(() => {
+    'use strict';
+    let arr = ['javascript', 'php', 'c', 'c++', 'java', 'python', 'ruby', 'c#', 'swift', 'objective-c'];
+    let inArray = (element, array) => {
+    return element === array.some()
+    }
+})();
