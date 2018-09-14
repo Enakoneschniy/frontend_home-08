65b61bd2db76a76cb5e019979b382990111912da
ivanov0ivanov <ivanov.ivanov8820@gmail.com>
2018-09-14 17:53:12 +0300

    HW

--- 12/task1/js/task1.js
+++ 12/task1/js/task1.js
@@ -1,5 +1,8 @@
-;(()=>{
+;(() => {
     'use strict';
-
-
+    const cont = document.querySelector('.container');
+    const coord = document.querySelector('.coordinates');
+    cont.addEventListener('mousemove', e => {
+        coord.textContent = `Х: ${e.clientX}, Y: ${e.clientY}`;
+    })
 })();
